54c94898329b3b97cf54ff88b9d3cd12bb93b3e5
lukevaughn20 <78869404+lukevaughn20@users.noreply.github.com>
2024-04-15 18:05:11 -0500

    Part 4 Assignment 13

--- routes/pizza.js
+++ routes/pizza.js
@@ -2,11 +2,19 @@ var express = require('express');
 const pizza_controller = require('../controllers/pizzas');
 var router = express.Router();
 
+const secured = (req,res,next)=>{
+    if(req.user){
+        return next();
+    }
+    res.redirect("/login");
+}
+
 /* GET home page. */
 router.get('/', pizza_controller.pizza_view_all_Page);
 router.get('/detail', pizza_controller.pizza_view_one_Page);
 router.get('/create', pizza_controller.pizza_create_Page);
-router.get('/update', pizza_controller.pizza_update_Page);
+router.get('/update', secured, pizza_controller.pizza_update_Page);
 router.get('/delete', pizza_controller.pizza_delete_Page);
+
 
 module.exports = router;
